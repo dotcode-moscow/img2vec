54e791bb5595a1f49f1745bb77f90427e98a8017
Stanislav Tsarkov <epzuz@yandex.ru>
2022-10-11 15:05:53 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, File, UploadFile
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from urllib.request import urlopen
+
+
+app = FastAPI()
+
+
+@app.post("/url")
+async def root(url: str):
+    img2vec = Img2Vec(cuda=True)
+    img = Image.open(urlopen(url)).convert('RGB')
+    vec = img2vec.get_vec(img)
+    return {"message": vec.tolist()}
+
+
+@app.post("/file")
+async def say_hello(file: UploadFile):
+    img2vec = Img2Vec(cuda=True)
+    img = Image.open(file.file).convert('RGB')
+    vec = img2vec.get_vec(img)
+    return {"message": vec.tolist()}
